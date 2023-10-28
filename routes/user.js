a1a61922ceeb0f50947e99456aff65debf8a76f7
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-10-28 18:31:20 +0500

    input per image only added

--- routes/user.js
+++ routes/user.js
@@ -1,26 +1,9 @@
 import express from "express"
 import { loginUser, logoutUser, updateUserProfile, deleteUserProfile, registerUser, getMyProfile, forgotPassword, resetPassword } from "../controller/user.js"
 import { isAuthenticated } from "../auth/isAuthenticated.js"
-import multer from "multer"
 
-const upload = multer()
+
 const router = express.Router()
-
-
-const validateImageUpload = (req, res, next) => {
-    ;
-    if (!req.file) {
-        return res.status(400).json({ message: 'No file uploaded' });
-    }
-
-    const fileMimeType = fileType(req.file.buffer);
-
-    if (!fileMimeType || !fileMimeType.mime.startsWith('image')) {
-        return res.status(400).json({ message: 'Only image files are allowed' });
-    }
-
-    next();
-};
 
 router.post("/user/register", registerUser)
 
