bc10167c9a73b2cfef92fff8bb5f6d6d32fff970
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-13 00:42:06 +0500

    Testimonials Schema Fixed

--- models/testimonial.js
+++ models/testimonial.js
@@ -8,10 +8,10 @@ const testimonialSchema = new mongoose.Schema({
         required: true,
         validate: {
             validator: function (value) {
-                // Use a regular expression to check if the value contains only alphabetic characters
-                return /^[a-zA-Z]+$/.test(value);
+                // Use a regular expression to check if the value contains only alphabetic characters and/or numeric values
+                return /^[a-zA-Z0-9]+$/.test(value);
             },
-            message: props => `${props.value} is not a valid text. Only alphabetic characters are allowed.`,
+            message: props => `${props.value} is not a valid string. Only alphabetic characters and numeric values are allowed.`,
         },
     },
     user: {
