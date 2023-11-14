97453e124a78b2ac82faf6067f9ba9a10adde343
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-15 01:43:44 +0500

    Logout Fixed

--- controller/user.js
+++ controller/user.js
@@ -134,9 +134,14 @@ export const getMyProfile = async (req, res) => {
 
 export const logoutUser = async (req, res) => {
     try {
-        return res.status(200).json({
+        return res.cookie("ghareebstar", null, {
+            expires: new Date(0),
+            httpOnly: true,
+            sameSite: "none",
+            secure: true,
+        }).status(200).json({
             success: true,
-            message: "Logged Out Successfully!"
+            message: "Logged Out Successfully"
         })
 
 
