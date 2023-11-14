9befc9262fccc8e6a0b8e356898de10780766c7a
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-14 12:17:32 +0500

    Logout Fixed Maybe

--- cookie/setCookie.js
+++ cookie/setCookie.js
@@ -1,10 +1,10 @@
 
 import jwt from "jsonwebtoken";
 const setCookie = async (user, res, message, statusCode = 200) => {
-  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_TOKEN_SECRET);
   res
     .status(statusCode)
-    .cookie("ecommerce", token, {
+    .cookie("ghareebstar", token, {
       maxAge: 15 * 24 * 60 * 60 * 1000,
       httpOnly: true,
       sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
