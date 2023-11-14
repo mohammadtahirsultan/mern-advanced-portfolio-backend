5da424104dfc65d587882a8e238d0b1df3f133c9
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-14 22:01:45 +0500

    Logout Fixed Maybe

--- controller/user.js
+++ controller/user.js
@@ -116,26 +116,12 @@ export const loginUser = async (req, res) => {
 
 }
 
-
-
 export const getMyProfile = async (req, res) => {
     try {
-
-        let user = await User.findById(req?.user?._id)
-
-        if (!user) {
-            return res.status(404).json({
-                success: false,
-                message: "User Not Found"
-            })
-        }
-
         return res.status(200).json({
             success: true,
-            user
+            user: req.user,
         })
-
-
     } catch (error) {
         return res.status(500).json({
             success: false,
