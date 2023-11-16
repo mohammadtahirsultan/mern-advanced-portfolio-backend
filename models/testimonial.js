f3be0f5ccd47f7240538806603d51af885fd1fd5
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-16 20:28:20 +0500

    Description Validation Removed

--- models/testimonial.js
+++ models/testimonial.js
@@ -6,13 +6,6 @@ const testimonialSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true,
-        validate: {
-            validator: function (value) {
-                // Use a regular expression to check if the value contains only alphabetic characters and/or numeric values
-                return /^[a-zA-Z0-9]+$/.test(value);
-            },
-            message: props => `${props.value} is not a valid string. Only alphabetic characters and numeric values are allowed.`,
-        },
     },
     user: {
         type: mongoose.Schema.Types.ObjectId,
