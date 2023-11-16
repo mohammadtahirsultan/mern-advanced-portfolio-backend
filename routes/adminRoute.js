726db48653ea93f59c4a8cb84024343782a2a0cb
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-11-16 12:13:59 +0500

    test category

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,7 +2,7 @@
 
 
 import express from "express"
-import {  getAllUsers, getUserDetail, updateUserProfileAdmin, deleteUserProfileAdmin } from "../controller/adminRoute.js"
+import { getAllUsers, getUserDetail, updateUserProfileAdmin, deleteUserProfileAdmin } from "../controller/adminRoute.js"
 import { isAdmin, isAuthenticated } from "../auth/isAuthenticated.js"
 import { addNewBlog, deleteBlog, updateBlog } from "../controller/blogs.js"
 import { addNewCategory, deleteCategory, editCategory } from "../controller/category.js"
@@ -29,13 +29,13 @@ router.delete("/blog/delete/:id", isAuthenticated, isAdmin, deleteBlog)
 router.put("/blog/:id", isAuthenticated, isAdmin, updateBlog)
 
 
-router.post("/category/create", addNewCategory)
+router.post("/category/create", isAuthenticated, isAdmin, addNewCategory)
 
-router.delete("/category/:id", deleteCategory)
+router.delete("/category/:id", isAuthenticated, isAdmin, deleteCategory)
 
-router.put("/category/:id", editCategory)
+router.put("/category/:id",isAuthenticated,isAdmin, editCategory)
 
-router.route("/user/:id").get(isAuthenticated, isAdmin, getUserDetail).put(isAuthenticated,isAdmin,updateUserProfileAdmin).delete(isAuthenticated, isAdmin, deleteUserProfileAdmin)
+router.route("/user/:id").get(isAuthenticated, isAdmin, getUserDetail).put(isAuthenticated, isAdmin, updateUserProfileAdmin).delete(isAuthenticated, isAdmin, deleteUserProfileAdmin)
 
 
 
